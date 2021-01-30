83fe65b37f405689ad0fe2c5853b1e259b336463
Adrian Cristea <adrian@klang-games.com>
2021-01-30 13:20:05 +0100

    Dog & level

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour {
+    public DogPushRadius PushRadius;
+    public DogPullRadius PullRadius;
+    
+    void Start() {
+    }
+
+    void Update() {
+    }
+    
+    
+}
---
+++ Assets/Scripts/DogPushRadius.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogPushRadius : MonoBehaviour
+{
+    private List<Sheep> sheeps = new List<Sheep>();
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Sheep")) {
+            sheeps.Add(other.GetComponent<Sheep>());
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other) {
+        
+    }
+}
