1aa1a84be07cb79bb0248777a086e9635665c0bb
Alessia Nigretti <alessia@klang-games.com>
2021-01-30 18:41:59 +0100

    Wolf logic

---
+++ Assets/Scripts/WolfSearchRadius.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WolfSearchRadius : MonoBehaviour
+{
+    private Wolf wolf;
+
+    private void Start()
+    {
+        wolf = GetComponentInParent<Wolf>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Sheep"))
+        {
+            wolf.SearchRadiusSheep.Add(other.gameObject);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Sheep"))
+        {
+            wolf.SearchRadiusSheep.Remove(other.gameObject);
+        }
+    }
+}
