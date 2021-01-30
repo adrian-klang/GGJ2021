0eaca602ddba63a8a7700078375b425767a62518
Adrian Cristea <adrian@klang-games.com>
2021-01-30 13:28:03 +0100

    Dog's pushing and pulling sheep

--- Assets/Scripts/DogPushRadius.cs
+++ Assets/Scripts/DogPushRadius.cs
@@ -3,17 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DogPushRadius : MonoBehaviour
-{
+public class DogPushRadius : MonoBehaviour {
+    public float Force = 10;
+    
     private List<Sheep> sheeps = new List<Sheep>();
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Sheep")) {
-            sheeps.Add(other.GetComponent<Sheep>());
+        var sheep = other.GetComponent<Sheep>();
+        if (sheep != null) {
+            sheeps.Add(sheep);
         }
     }
 
     public void OnTriggerExit2D(Collider2D other) {
-        
+        var sheep = other.GetComponent<Sheep>();
+        if (sheep != null) {
+            sheeps.Remove(sheep);
+        }
+    }
+
+    public void Apply() {
+        foreach (var sheep in sheeps) {
+            var f = sheep.transform.position - transform.position;
+            f *= Force;
+            sheep.Rigidbody.AddForce(f);
+        }
     }
 }
