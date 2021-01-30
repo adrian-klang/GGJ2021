615b15ee9f5c20ea1021d3965b496667bf4c475b
Adrian Cristea <adrian@klang-games.com>
2021-01-30 17:09:53 +0100

    optimisations

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -6,86 +7,13 @@ using UnityEngine;
 /// </summary>
 public class Sheep : MonoBehaviour
 {
-    public GameConfig Config;
     public Rigidbody2D Rigidbody;
-    public CircleCollider2D inner, mid, outer;
-    
-    [HideInInspector]
-    public List<GameObject> InnerCollisions = new List<GameObject>();
-    [HideInInspector]
-    public List<GameObject> MidCollisions = new List<GameObject>();
-    [HideInInspector]
-    public List<GameObject> OuterCollisions = new List<GameObject>();
 
-    private void FixedUpdate()
-    {
-        if (InnerCollisions.Count > 0)
-        {
-            AddForceInner();
-        }
-
-        if (MidCollisions.Count > 0)
-        {
-            AddForceMid();
-        }
-
-        if (OuterCollisions.Count > 0)
-        {
-            AddForceOuter();
-        }
-        
-        inner.radius = Config.SeparationRadius;
-        mid.radius = Config.AlignmentRadius;
-        outer.radius = Config.CohesionRadius;
+    private void OnEnable() {
+        SheepManager.RegisterSheep(this);
     }
 
-    private void AddForceInner()
-    {
-        Vector3 avgPosition = Vector3.zero;
-
-        foreach (var sheep in InnerCollisions)
-        {
-            avgPosition += sheep.transform.position;
-        }
-
-        avgPosition /= InnerCollisions.Count;
-        
-        var forceToApply = (transform.position - avgPosition).normalized * Config.SeparationForce;
-        
-        Rigidbody.AddForce(forceToApply);
-    }
-    
-    private void AddForceMid()
-    {
-        Vector2 avgVelocity = Vector2.zero;
-
-        foreach (var sheep in MidCollisions)
-        {
-            // todo: remove getcomponent
-            var rigidbody = sheep.GetComponentInParent<Rigidbody2D>();
-            avgVelocity += rigidbody.velocity;
-        }
-
-        avgVelocity /= MidCollisions.Count;
-
-        var forceToApply = avgVelocity * Config.AlignmentForce;
-        
-        Rigidbody.AddForce(forceToApply);
-    }
-    
-    private void AddForceOuter()
-    {
-        Vector3 avgPosition = Vector3.zero;
-
-        foreach (var sheep in OuterCollisions)
-        {
-            avgPosition += sheep.transform.position;
-        }
-
-        avgPosition /= OuterCollisions.Count;
-
-        var forceToApply = (avgPosition - transform.position).normalized * Config.CohesionForce;
-
-        Rigidbody.AddForce(forceToApply);
+    private void OnDisable() {
+        SheepManager.DeregisterSheep(this);
     }
 }
---
+++ Assets/Scripts/SheepManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepManager : MonoBehaviour {
+    private static List<Sheep> Sheeps = new List<Sheep>(128);
+
+    public GameConfig Config;
+
+    private float separationSqrRadius;
+    private float alignmentSqrRadius;
+    private float cohesionSqrRadius;
+    
+    private List<Vector2> separationVectors = new List<Vector2>();
+    private List<Vector2> alignmentVectors = new List<Vector2>();
+    private List<Vector2> cohesionVectors = new List<Vector2>();
+    
+    public static void RegisterSheep(Sheep sheep) {
+        Sheeps.Add(sheep);
+    }
+
+    public static void DeregisterSheep(Sheep sheep) {
+        Sheeps.Remove(sheep);
+    }
+
+    private void Update() {
+        separationSqrRadius = Config.SeparationRadius * Config.SeparationRadius;
+        alignmentSqrRadius = Config.AlignmentRadius * Config.AlignmentRadius;
+        cohesionSqrRadius = Config.CohesionRadius * Config.CohesionRadius;
+    }
+
+    private void FixedUpdate() {
+        foreach (var sheepOne in Sheeps) {
+            
+            separationVectors.Clear();
+            alignmentVectors.Clear();
+            cohesionVectors.Clear();
+            
+            Vector2 posOne = sheepOne.transform.position;
+            
+            foreach (var sheepTwo in Sheeps) {
+                if (sheepOne == sheepTwo) {
+                    continue;
+                }
+                
+                Vector2 posTwo = sheepTwo.transform.position;
+
+                var sqrDist = (posOne.x - posTwo.x) * (posOne.x - posTwo.x) + (posOne.y - posTwo.y) * (posOne.y - posTwo.y);
+
+                if (sqrDist < separationSqrRadius) {
+                    separationVectors.Add(posTwo);
+                }
+
+                if (sqrDist < alignmentSqrRadius) {
+                    alignmentVectors.Add(sheepTwo.Rigidbody.velocity.normalized);
+                }
+
+                if (sqrDist < cohesionSqrRadius) {
+                    cohesionVectors.Add(posTwo);
+                }
+            }
+
+            var totalForce = Vector2.zero;
+
+            if (cohesionVectors.Count > 0) {
+                var avgPos = Vector2.zero;
+                foreach (var vector2 in cohesionVectors) {
+                    avgPos += vector2;
+                }
+
+                avgPos /= cohesionVectors.Count;
+
+                totalForce += (avgPos - posOne).normalized * Config.CohesionForce;
+            }
+            
+            
+            if (separationVectors.Count > 0) {
+                var avgPos = Vector2.zero;
+                foreach (var vector2 in separationVectors) {
+                    avgPos += vector2;
+                }
+
+                avgPos /= separationVectors.Count;
+
+                totalForce += (posOne - avgPos).normalized * Config.SeparationForce;
+            }
+
+            if (alignmentVectors.Count > 0) {
+                var avgDir = Vector2.zero;
+                foreach (var vector2 in alignmentVectors) {
+                    avgDir += vector2;
+                }
+
+                avgDir /= alignmentVectors.Count;
+
+                totalForce += avgDir.normalized * Config.AlignmentForce;
+            }
+            
+            sheepOne.Rigidbody.AddForce(totalForce);
+        }
+    }
+}
