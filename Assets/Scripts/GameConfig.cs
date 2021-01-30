054dacfce92bbeccb7a7104a1b9f3209ebf47ec6
Alessia Nigretti <alessia@klang-games.com>
2021-01-30 17:56:15 +0100

    Wolf config

--- Assets/Scripts/GameConfig.cs
+++ Assets/Scripts/GameConfig.cs
@@ -13,13 +13,16 @@ public class GameConfig : ScriptableObject {
     public float DogPullForce = 10;
     public float DogPullRadius = 10;
     
-    [Header("Sheep Forces")]
+    [Header("Sheep")]
     public float SeparationForce = 1f;
     public float AlignmentForce = 1f;
     public float CohesionForce = 1f;
-    
-    [Header("Sheep Radii")]
     public float SeparationRadius = 1f;
     public float AlignmentRadius = 2f;
     public float CohesionRadius = 3f;
+
+    [Header("Wolf")]
+    public float FollowRadius = 3f;
+    public float SearchRadius = 10f;
+    public float WolfForce = 5f;
 }
